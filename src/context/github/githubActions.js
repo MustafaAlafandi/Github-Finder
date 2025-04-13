f6bed520b2d184c6add769df32090820a6e37d9c
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-04-13 23:08:03 +0300

    remove token

--- src/context/github/githubActions.js
+++ src/context/github/githubActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const github = axios.create({
   baseURL: process.env.REACT_APP_GITHUB_URL,
-  headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
+  // headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
   method:"GET",
 });
 export const searchUsers = async (text) => {
